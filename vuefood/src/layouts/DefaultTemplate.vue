03ea64d2785c88a077a1c71cf05d11d8364329d5
Fabio <fabio.drioli@gmail.com>
2021-09-22 12:31:22 -0300

    Criação das rotas paginas, header e footer

--- vuefood/src/layouts/DefaultTemplate.vue
+++ vuefood/src/layouts/DefaultTemplate.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-        sdfds
+        <header-component/>
+        <div class="container container-body">
+          <router-view></router-view>
+        </div>
+        <footer-component/>
     </div>
 </template>
 
 <script>
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import HeaderComponent from './_partials/header.vue'
+import FooterComponent from './_partials/footer.vue'
 export default {
-    setup() {
-        
-    },
+  data () {
+    return {
+    }
+  },
+  components:{
+      HeaderComponent,
+      FooterComponent,
+  }
 }
 </script>
+
+<style scoped>
+    @import url('../assets/css/vuefood.css');
+    @import url('../assets/css/reset.css');
+</style>
---
+++ vuefood/src/routes/routes.map.js
@@ -0,0 +1,23 @@
+import Home from '@/pages/Home'
+import Products from '@/pages/Products'
+import Cart from '@/pages/Cart'
+
+const routes = [
+    {
+        path:'/',
+        component:Home,
+        name:'home'
+    },
+    {
+        path:'/loja',
+        component:Products,
+        name:'products'
+    },
+    {
+        path:'/cart',
+        component:Cart,
+        name:'cart'
+    }
+]
+
+export default routes
